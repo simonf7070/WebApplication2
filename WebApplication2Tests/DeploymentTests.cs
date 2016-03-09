e5ecea8b3723aba0b89fa428ea7f5ec49f7eefb9
simonf7070 <simonf7070@gmail.com>
2016-03-09 16:03:10 +0000

    added test project

---
+++ WebApplication2Tests/DeploymentTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace WebApplication2Tests
+{
+    [TestFixture]
+    public class DeploymentTests
+    {
+        [Test]
+        public void DeploymentTest()
+        {
+            Assert.That(true, Is.False);
+        }
+    }
+}
